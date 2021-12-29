46ec1d9809488fe79ac17bb7670ef5187550ccb3
mhdabd_ <razorext2@gmail.com>
2021-12-30 00:41:16 +0700

    adding template folder

---
+++ split/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer pt-3 no-print">
+    <div class="container-fluid">
+        <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+                <div class="copyright text-center text-sm text-muted text-lg-start">
+                    Copyright &COPY; <?= date('Y') ?> Developer Kampoeng |
+                    <a href="#">Dokumentasi</a>
+                    <a href="#">Peta Situs</a>
+                    <a href="#">Support</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
